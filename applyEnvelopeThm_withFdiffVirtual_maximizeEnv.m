6d7b7d0a36d616ebc4378e76c84cb18604d50c6a
Paul Zhang <pzpzpzp1@trick.inf.unibe.ch>
2022-04-27 20:34:59 +0200

    add minimization of dLda wrt full env thm.

--- applyEnvelopeThm_withFdiffVirtual_maximizeEnv.m
+++ applyEnvelopeThm_withFdiffVirtual_maximizeEnv.m
@@ -61,61 +61,27 @@ for j = 1:niie
     u3 = u(t3,:)';
     v3 = v(t3,:)';
 
-    %% build perturbation frames
-    eps = 1e-4; % 1e-5, 1e-4 seem ok. too small and hit optimality tolerance too quick obtaining inaccurate answer. too large and it never finishes solving.
-    z = zeros(1,2);
-    Am = [e1v' z z z; z z e1v' z; z e2v' z z; z z z e2v'; z ehat' ehat' z; -ehat' z z ehat'];
-    Bm = [dot(u0,e1v); dot(v0,e1v); dot(u2,e2v); dot(v2,e2v); 0; 0];
-    e3p = e1v + eps * ehat;
-    e4p = e2v + eps * ehat;
-    e1p = e1v;    e2p = e2v;
-    ehp = ehat;
-    A = [e1p' z z z; z z e1p' z; z e2p' z z; z z z e2p'; z ehp' ehp' z; -ehp' z z ehp';...
-        e3p' z z z; z z e3p' z; z e4p' z z; z z z e4p'];
-    B = [dot(u0,e1p); dot(v0,e1p); dot(u2,e2p); dot(v2,e2p); 0; 0;...
-        dot(u1,e3p); dot(v1,e3p); dot(u3,e4p); dot(v3,e4p); ];
-    xperturb = A\B;
+    %% get dual vars
+    if data.edges2triangles(e1,1)~=t1
+        l1flip=~l1flip;
+    end
+    if data.edges2triangles(e2,1)~=t3
+        l2flip=~l2flip;
+    end
+    l1u = lambda.eqlin(e1);
+    l1v = lambda.eqlin(e1+nie);
+    l2u = lambda.eqlin(e2);
+    l2v = lambda.eqlin(e2+nie);
+    if l1flip;
+        l1u = -l1u;
+        l1v = -l1v;
+    end
+    if l2flip;
+        l2u = -l2u;
+        l2v = -l2v;
+    end
 
-    %% build new mesh topology
-    newv = data.vertices(p1,:) + eps*[ehat' 0];
-    newvind = data.numVertices+1;
-    Vnew = [V0; newv]; 
-    Tnew = T;     Tnew(t1,:)=[p2 p4  newvind];    Tnew(t3,:)=[p6 p2 newvind];    Tnew = [Tnew; p4 p1 newvind ; p1 p6 newvind ];
-    permuteEdgeVinds = [p1 newvind];
-    x0 = reshape([reshape(x,[],2); reshape(xperturb,[],2)],[],1);
-    [newX,newU,newV,~,~,newData,newLambda,fval1,C,exitflag(j)] = computePolyvectorField(Vnew,Tnew,optol,x0,permuteEdgeVinds);
-    fdiffs(j) = (fval1-fval0)/eps; 
-    % [intedgeind=201: -.17 | gt =  -0.47667     -0.86683     -0.50003   -0.0069196      0.61278      -4.1552     -0.40546      -3.4958] 
-    % [intedgeind=3044: -34.112 | gt = -31.896       4.2846      -31.552      -8.3002       35.544      -6.0746      -28.372      -8.4041] 
-
-    xr = reshape(newX,[],2);
-    gt = reshape(xr(end-3:end,:),[],1);
-    
-    %TODO: CHECK LAMBDAS. TRY OPTIMIZING dL/dalpha w.r.t. virtual frames
-
-    %% set primal virtual frames (this is the key part to test)
-    % lopital's rule area ratio
-    %{
-    A4_div_A5 = abs(norm(cross([e1v' 0],[ehat' 0]))/norm(cross([e2v' 0],[ehat' 0])));
-    fun = @(x)obfun_wrapper(x,[A4_div_A5 1]);
-    options = optimoptions('fmincon','Display','Iter','CheckGradients',false,'SpecifyObjectiveGradient',true,'Algorithm','interior-point','HessianApproximation','lbfgs','SpecifyConstraintGradient',true,...
-        'OptimalityTolerance',optol);
-    xl = fmincon(fun,x0,[],[],Am,Bm,[],[],[],options); % use min energy frames
-    %}
-    xl = gt; % use gt frames
-%     xl = x0; % use perturbation frames
-    u4 = xl(1:2);
-    u5 = xl(3:4);
-    v4 = xl(5:6);
-    v5 = xl(7:8);
-    
-    uv4 = [u4 v4];
-    uv5 = [u5 v5];
-    E4 = symmetricDirichlet(uv4,1);
-    E5 = symmetricDirichlet(uv5,1);
-    E45c = [E4 E5];
-    
-    %% Energy envelope gradient
+    %% get dAda
     t12inds = [t1, t3];
     nt = numel(t12inds);
     for i=1:nt
@@ -138,63 +104,65 @@ for j = 1:niie
         Ai(i) = areaFromV123(v123);
     end
     dAda = [double(diff(Ai,alpha))]/sum(data0.triangleAreas);
-    adiff_part1(j) = dot(dAda, [Eic E45c]);
 
-    %% get constraint envelope gradient
-    if data.edges2triangles(e1,1)~=t1
-        l1flip=~l1flip;
-    end
-    if data.edges2triangles(e2,1)~=t3
-        l2flip=~l2flip;
-    end
-    l1u = lambda.eqlin(e1);
-    l1v = lambda.eqlin(e1+nie);
-    l2u = lambda.eqlin(e2);
-    l2v = lambda.eqlin(e2+nie);
-    if l1flip;
-        l1u = -l1u;
-        l1v = -l1v;
-    end
-    if l2flip;
-        l2u = -l2u;
-        l2v = -l2v;
-    end
-    adiff_part2(j) = l1u*dot(u1-u4,ehat) + l1v*dot(v1-v4,ehat) + l2u*dot(u3-u5,ehat) + l2v*dot(v3-v5,ehat);
+    %% build perturbation frames
+    eps = 1e-4; % 1e-5, 1e-4 seem ok. too small and hit optimality tolerance too quick obtaining inaccurate answer. too large and it never finishes solving.
+    z = zeros(1,2);
+    Am = [e1v' z z z; z z e1v' z; z e2v' z z; z z z e2v'; z ehat' ehat' z; -ehat' z z ehat'];
+    Bm = [dot(u0,e1v); dot(v0,e1v); dot(u2,e2v); dot(v2,e2v); 0; 0];
+    e3p = e1v + eps * ehat;
+    e4p = e2v + eps * ehat;
+    e1p = e1v;    e2p = e2v;
+    ehp = ehat;
+    A = [e1p' z z z; z z e1p' z; z e2p' z z; z z z e2p'; z ehp' ehp' z; -ehp' z z ehp';...
+        e3p' z z z; z z e3p' z; z e4p' z z; z z z e4p'];
+    B = [dot(u0,e1p); dot(v0,e1p); dot(u2,e2p); dot(v2,e2p); 0; 0;...
+        dot(u1,e3p); dot(v1,e3p); dot(u3,e4p); dot(v3,e4p); ];
+    xperturb = A\B;
 
-    %% get xperturb that minimizes env grad: dLda
+    %% build ground truth frames
     %{
-    fun = @(x)obfun_wrapper(x, 1);
-    xo=Am\Bm;N=null(Am); dt=1e-1;
-    uv45 = gt;
-    for t=1:500
-        % project onto nullspace
-        n=N\(uv45-xo);
-        uv45 = xo+N*n;
-        u4 = uv45(1:2);
-        u5 = uv45(3:4);
-        v4 = uv45(5:6);
-        v5 = uv45(7:8);
-
-        % get d_dLdalpha_d_uv45
-        [~,dEduv4] = fun([u4;v4]);
-        [~,dEduv5] = fun([u5;v5]);
-%         duv4 = dEduv4*dAda(3) - (l1u*[ehat; z'] + l1v*[z'; ehat]);
-%         duv5 = dEduv5*dAda(4) - (l2u*[ehat; z'] + l2v*[z'; ehat]);
-%         duv4 = dEduv4*dAda(3);
-%         duv5 = dEduv5*dAda(4);
-        duv4 =  - (l1u*[ehat; z'] + l1v*[z'; ehat]);
-        duv5 =  - (l2u*[ehat; z'] + l2v*[z'; ehat]);
-        duv45 = reshape([reshape(duv4,2,2); reshape(duv5,2,2); ],[],1);
-        
-        % project onto tangent space
-        duv45 = N*(N\duv45);
-
-        % step
-        uv45 = uv45 + dt * duv45;
-        norm(uv45-gt)
-    end
-    uv45_min_dLda = uv45;
+    newv = data.vertices(p1,:) + eps*[ehat' 0];
+    newvind = data.numVertices+1;
+    Vnew = [V0; newv]; 
+    Tnew = T;     Tnew(t1,:)=[p2 p4  newvind];    Tnew(t3,:)=[p6 p2 newvind];    Tnew = [Tnew; p4 p1 newvind ; p1 p6 newvind ];
+    permuteEdgeVinds = [p1 newvind];
+    x0 = reshape([reshape(x,[],2); reshape(xperturb,[],2)],[],1);
+    [newX,newU,newV,~,~,newData,newLambda,fval1,C,exitflag(j)] = computePolyvectorField(Vnew,Tnew,optol,x0,permuteEdgeVinds);
+    fdiffs(j) = (fval1-fval0)/eps; 
+    % [intedgeind=201: -.17 | gt =  -0.47667     -0.86683     -0.50003   -0.0069196      0.61278      -4.1552     -0.40546      -3.4958] 
+    % [intedgeind=3044: -34.112 | gt = -31.896       4.2846      -31.552      -8.3002       35.544      -6.0746      -28.372      -8.4041] 
+    xr = reshape(newX,[],2);
+    gt = reshape(xr(end-3:end,:),[],1);
     %}
+    
+    %% set primal virtual frames 
+    %{
+    A4_div_A5 = abs(norm(cross([e1v' 0],[ehat' 0]))/norm(cross([e2v' 0],[ehat' 0]))); % lopital
+    fun = @(x)obfun_wrapper(x,[A4_div_A5 1]);
+    options = optimoptions('fmincon','Display','Iter','CheckGradients',false,'SpecifyObjectiveGradient',true,'Algorithm','interior-point','HessianApproximation','lbfgs','SpecifyConstraintGradient',true,...
+        'OptimalityTolerance',optol);
+    xl = fmincon(fun,x0,[],[],Am,Bm,[],[],[],options); % use min energy frames
+    %}
+%     xl = gt; % use gt frames
+%     xl = xperturb; % use perturbation frames
+    
+    % get xl that minimizes env grad: dLda
+    options = optimoptions('fmincon','Display','Iter','CheckGradients',false,'SpecifyObjectiveGradient',true,'Algorithm','interior-point','HessianApproximation','lbfgs',...
+        'OptimalityTolerance',optol,'ConstraintTolerance',1e-14);
+    obfun = @(xx) dLda_part(dAda, @(x)obfun_wrapper(x, 1), [l1u l1v l2u l2v], ehat, xx);
+    [xl,~,exitflag,~,~,grad,hessian] = fmincon(obfun,xperturb,[],[],Am,Bm,[],[],[],options);
+
+    %% envelope gradient
+    extraIn.u1=u1;
+    extraIn.v1=v1;
+    extraIn.u3=u3;
+    extraIn.v3=v3;
+    extraIn.Eic=Eic;
+    [~, ~, extraOut] = dLda_part(dAda, @(x)obfun_wrapper(x, 1), [l1u l1v l2u l2v], ehat, xl, extraIn);
+
+    adiff_part1(j) = extraOut.val1;
+    adiff_part2(j) = extraOut.val2;
 
     %% break at set percentage
 %     if j/niie > .03758
---
+++ dLda_part.m
@@ -0,0 +1,43 @@
+function [val, grad, extra] = dLda_part(dAda, fun, ls, ehat, x0, extraIn)
+    if exist('extraIn','var')
+        u1 = extraIn.u1;
+        v1 = extraIn.v1;
+        u3 = extraIn.u3;
+        v3 = extraIn.v3;
+        Eic = extraIn.Eic;
+    else
+        u1 = [0;0];
+        v1 = [0;0];
+        u3 = [0;0];
+        v3 = [0;0];
+        Eic = [0 0];
+    end
+
+
+    uv45 = x0;
+    u4 = uv45(1:2);
+    u5 = uv45(3:4);
+    v4 = uv45(5:6);
+    v5 = uv45(7:8);
+    l1u = ls(1);
+    l1v = ls(2);
+    l2u = ls(3);
+    l2v = ls(4);
+    [E4,dEduv4] = fun([u4;v4]);
+    [E5,dEduv5] = fun([u5;v5]);
+
+    val1 = dot(dAda, [Eic E4 E5]);
+    val2 = l1u*dot(u1-u4,ehat) + l1v*dot(v1-v4,ehat) + l2u*dot(u3-u5,ehat) + l2v*dot(v3-v5,ehat);
+    val = val1+val2;
+    
+    z = [0 0];
+    duv4 = dEduv4*dAda(3) - (l1u*[ehat; z'] + l1v*[z'; ehat]);
+    duv5 = dEduv5*dAda(4) - (l2u*[ehat; z'] + l2v*[z'; ehat]);
+    duv45 = reshape([reshape(duv4,2,2); reshape(duv5,2,2); ],[],1);
+    grad = duv45;
+    
+    extra.val1 = val1;
+    extra.val2 = val2;
+    extra.dEduv4 = dEduv4;
+    extra.dEduv5 = dEduv5;
+end
